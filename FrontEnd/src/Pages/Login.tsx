6bf35e2532fd018852bf0bcdb03e90c39e3bbad9
Shucayb_Ayuub <shucayb594@gmail.com>
2025-11-23 20:36:04 +0300

    make validated

--- FrontEnd/src/Pages/Login.tsx
+++ FrontEnd/src/Pages/Login.tsx
@@ -6,51 +6,49 @@ import G_logo from "../assets/google.png";
 import F_logo from "../assets/facebook.png";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
-import { error } from "console";
+
 type user = {
   Email: string;
   password: string;
 };
 const Login = () => {
-  const [Userinput, setUserinput] = useState<user>({Email : "" , password: ""});
-  const [Error , setError] = useState({email : "" , password:""})
+  const [Userinput, setUserinput] = useState<user>({ Email: "", password: "" });
+  const [Error, setError] = useState({ email: "", password: "" });
 
   const Validate = () => {
     let valid = true;
-    let newError = {email : "" , password : ""}
+    let newError = { email: "", password: "" };
 
     if (!Userinput.Email.includes("@")) {
-      newError.email  = "Please Enter valid email"
-      valid = false
+      newError.email = "Please Enter valid email";
+      valid = false;
     }
+
+    const symbolRegex = /[!@#$%^&*(),.?":{}|]/;
 
     if (Userinput.password.length < 7) {
-        newError.password = "Password must be at least 7 character"
-        valid = false
+      newError.password = "Password must be at least 7 character";
+      valid = false;
+    } else if (!symbolRegex.test(Userinput.password)) {
+      newError.password =
+        "Password must contains at least one symbol (!@#$%^&*)";
+      valid = false;
     }
 
-    const symbolRegex = /[!@#$%^&*(),.?":{}|]/
+    setError(newError);
+    return valid;
+  };
 
-    if (!symbolRegex.test(Userinput.password)) {
-      newError.password = "Password must contains at least one symbol (!@#$%^&*)"
-      valid = false
+  const Submit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (Validate()) {
     }
-
-    setError(newError)
-    return valid
-  }
-
-  const Submit = (e:React.FormEvent) => {
-     e.preventDefault()
-
-     if (Validate()) {
-      
-     }
-  }
+  };
 
   return (
-    <div className=" sm:h-30 md:h-52 lg:h-screen ">
-      <div className="flex flex-col justify-center items-center my-auto mx-auto p-4 bg-white w-xs h-170   sm:w-md shadow-lg">
+    <div className=" sm:h-30 md:h-52 lg:min-h-screen ">
+      <div className="flex flex-col justify-center items-center my-auto mx-auto p-4 bg-white w-xs h-170   sm:w-md shadow-lg pb-4">
         <div className="p-3  flex flex-col justify-center">
           <img src={Logo} alt="" className="w-20 rounded-4xl mx-auto" />
           <h1 className="font-bold text-2xl text-center">Task Manager</h1>
@@ -75,34 +73,43 @@ const Login = () => {
           className="w-full flex-col justify-center space-y-4 mt-4"
         >
           <div className="flex flex-col space-y-3">
-            <label htmlFor="" className="font-semibold">Email</label>
+            <label htmlFor="" className="font-semibold">
+              Email
+            </label>
 
             <input
               type="text"
               name="Email"
               placeholder="you@example.com"
               value={Userinput.Email}
-
-              onChange={(e) => setUserinput({...Userinput , Email : e.target.value})}
+              onChange={(e) =>
+                setUserinput({ ...Userinput, Email: e.target.value })
+              }
               className="h-9 rounded-md px-3  bg-gray-200 
               focus:outline-none  focus:ring-2 focus:ring-purple-500 focus:ring-offset-1 "
             />
 
-            {Error.email && <p className="text-red-400 text-sm">{Error.email}</p>}
+            {Error.email && (
+              <p className="text-red-500 text-md">{Error.email}</p>
+            )}
           </div>
           <div className="flex flex-col space-y-3">
-            <label htmlFor="" className="font-semibold">Password</label>
+            <label htmlFor="" className="font-semibold">
+              Password
+            </label>
             <input
-              type = "password"
+              type="password"
               name="Password"
               placeholder="password"
               value={Userinput.password}
-              onChange={(e) => setUserinput({...Userinput , password:e.target.value})}
+              onChange={(e) =>
+                setUserinput({ ...Userinput, password: e.target.value })
+              }
               className="h-9 border-2 px-3 rounded-md bg-gray-200 focus:outline-none 
               focus:ring-2 focus:ring-purple-500 focus:ring-offset-1"
             />
 
-            {Error && <p className="text-red-500 text-sm">{Error.password}</p>}
+            {Error && <p className="text-red-500 text-md">{Error.password}</p>}
             <Link to="/" className="ms-auto text-blue-600 font-semibold">
               Forget password?
             </Link>
@@ -111,13 +118,14 @@ const Login = () => {
             <Button
               size={"lg"}
               variant={"link"}
+              type="submit"
               className="w-70 sm:w-sm bg-purple-500 text-white text-lg "
             >
               Submit
             </Button>
             <br />
           </div>
-          <p className="text-center mt-4 pt-4 text-gray-500 font-medium">
+          <p className="text-center mt-4 pt-4 text-gray-500 font-medium pb-5">
             Don't have an account?{" "}
             <Link to="/Singup" className="text-purple-600">
               Sing up
--- FrontEnd/src/Pages/SingUp.tsx
+++ FrontEnd/src/Pages/SingUp.tsx
@@ -1,17 +1,43 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/taskmanager-logo.png";
-
+import  {AiOutlineEye , AiOutlineEyeInvisible } from 'react-icons/ai'
 import G_logo from "../assets/google.png";
 import F_logo from "../assets/facebook.png";
 import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
+
 type user = {
-  Email: string;
+  confirm: string;
   password: string;
+  showPass : boolean
 };
 const SingUp = () => {
-  const [Userinput, setUserinput] = useState<user>();
+  const [Userinput, setUserinput] = useState<user>({
+    confirm: "",
+    password: "",
+    showPass:false
+  });
+
+  const [showPassword  , setShow] = useState<boolean>(false)
+  let valid = true;
+
+  const [Error, setError] = useState<string>("");
+
+  const Validate = () => {
+    if (Userinput.password !== Userinput.confirm) {
+      setError("Password must match");
+      valid = false;
+    }
+
+    return valid;
+  };
+
+  const Submit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (Validate()) {
+    }
+  };
   return (
     <div className=" sm:h-30 md:h-52 lg:h-screen ">
       <div className="flex flex-col justify-center items-center my-auto mx-auto p-4 bg-white w-xs h-210   sm:w-md shadow-lg">
@@ -36,11 +62,13 @@ const SingUp = () => {
         </div>
         <form
           action=""
+          onSubmit={Submit}
           className="w-full flex-col justify-center space-y-4 mt-4"
         >
-
           <div className="flex flex-col space-y-3">
-            <label htmlFor="" className="font-semibold">Name</label>
+            <label htmlFor="" className="font-semibold">
+              Name
+            </label>
 
             <input
               type="text"
@@ -52,7 +80,9 @@ const SingUp = () => {
           </div>
 
           <div className="flex flex-col space-y-3">
-            <label htmlFor="" className="font-semibold">Email</label>
+            <label htmlFor="" className="font-semibold">
+              Email
+            </label>
 
             <input
               type="text"
@@ -62,33 +92,49 @@ const SingUp = () => {
               focus:outline-none  focus:ring-2 focus:ring-purple-500 focus:ring-offset-1 "
             />
           </div>
-          
-          <div className="flex flex-col space-y-3">
-            <label htmlFor="" className="font-semibold">Password</label>
+
+          <div className="flex flex-col space-y-3 relative">
+            <label htmlFor="" className="font-semibold">
+              Password
+            </label>
             <input
-              type="text"
+              type={Userinput.showPass ? "text" : "Password"}
               name="Password"
               placeholder="password"
+              
+              value={Userinput.password}
+              onChange={(e) => setUserinput({...Userinput , password:e.target.value})}
+
               className="h-9 border-2 px-3 rounded-md bg-gray-200 focus:outline-none 
               focus:ring-2 focus:ring-purple-500 focus:ring-offset-1"
             />
-           
+
+            <div className="absolute right-2 top-11 cursor-pointer" onClick={() => setUserinput({...Userinput , showPass:!Userinput.showPass})}>
+              {Userinput.showPass  ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
+            </div>
           </div>
           <div className="flex flex-col space-y-3">
-            <label htmlFor="" className="font-semibold">Confirm Password</label>
+            <label htmlFor="" className="font-semibold">
+              Confirm Password
+            </label>
             <input
-              type="text"
+              type="Password"
               name="Password"
               placeholder="password"
+              value={Userinput.confirm}
+              onChange={(e) =>
+                setUserinput({ ...Userinput, confirm: e.target.value })
+              }
               className="h-9 border-2 px-3 rounded-md bg-gray-200 focus:outline-none 
               focus:ring-2 focus:ring-purple-500 focus:ring-offset-1"
             />
-          
+            {Error && <p className="text-red-500 ">{Error}</p>}
           </div>
           <div className="flex justify-center">
             <Button
               size={"lg"}
               variant={"link"}
+              type="submit"
               className="w-70 sm:w-sm bg-purple-500 text-white text-lg "
             >
               Submit
